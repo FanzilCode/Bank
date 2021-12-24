ccc25f6f6276c5354f339d7d5a00e05c49b25906
Фанзиль Тимершин <fanzil.timershin@mail.ru>
2021-12-24 12:16:34 +0500

    Добавьте файлы проекта.

---
+++ Bank/Client.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace Bank
+{
+    class Client
+    {
+        // название
+        string title;
+        // вид собственности
+        string property;
+        // адрес
+        string address;
+        // телефон
+        string phone;
+        // контактное лицо
+        string person;
+
+        // конструктор с 5-ю параметрами
+        public Client(string title, string property, string address, string phone, string person)
+        {
+            this.title = title;
+            this.property = property;
+            this.address = address;
+            this.phone = phone;
+            this.person = person;
+        }
+        // конструктор без параметров
+        public Client()
+        {
+            Console.Write("Название: ");
+            title = Console.ReadLine();
+
+            Console.Write("\nВид собственнности: ");
+            property = Console.ReadLine();
+
+            Console.Write("\nАдрес: ");
+            address = Console.ReadLine();
+
+            Console.Write("\nТелефон: ");
+            phone = Console.ReadLine();
+
+            Console.Write("\nКонтактное лицо: ");
+            person = Console.ReadLine();
+        }
+        // конструктор для чтения из файла
+        public Client(string[] arr)
+        {
+            title = arr[0];
+            property = arr[1];
+            address = arr[2];
+            phone = arr[3];
+            person = arr[4];
+        }
+
+        // переопредениие метода ToString() для записи в файл
+        public override string ToString()
+        {
+            return $"{title}%{property}%{address}%{phone}%{person}";
+        }
+        // метод Print() для выведения на экран информации о клиенте
+        public void Print()
+        {
+            Console.WriteLine($"Название: {title}\n" +
+                $"Вид собственности: {property}\n" +
+                $"Адрес: {address}\n" +
+                $"Телефон: {phone}\n" +
+                $"Контактное лицо: {person}");
+        }
+        // операторы равенства
+        public static bool operator ==(Client c1, Client c2)
+        {
+            return (c1.title == c2.title && c1.property == c2.property && c1.address == c2.address && c1.phone == c2.phone && c1.person == c2.person);
+        }
+        public static bool operator !=(Client c1, Client c2)
+        {
+            return !(c1 == c2);
+        }
+        // переопределение метода Equals()
+        public override bool Equals(object obj)
+        {
+            if (obj.GetType() != this.GetType())
+                return false;
+            Client client = (Client)obj;
+            return this == client;
+        }
+        // переопределение метода GetHashCode()
+        public override int GetHashCode()
+        {
+            return title.GetHashCode() + property.GetHashCode() + address.GetHashCode() + phone.GetHashCode() + phone.GetHashCode() + person.GetHashCode();
+        }
+    }
+}
